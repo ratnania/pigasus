f60a5d42d3b14529c4ac0760b2c950741bedab6f
Ahmed Ratnani <ratnaniahmed@gmail.com>
2014-10-27 11:38:27 +0100

    remove src directory. it was replaced by fortran and python sources directories

--- src/pigasus/gallery/__init__.py
+++
@@ -1,7 +0,0 @@
-"""A pigasus demonstration vehicle.
-
-.. moduleauthor:: Ahmed Ratnani <ratnaniahmed@gmail.com>
-
-"""
-
-import poisson
--- src/pigasus/utils/manager.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: UTF-8 -*-
-class context(object):
-    def __init__(self):
-        pass
-    def __enter__(self):
-        pass
-    def __exit__(self, type, value, tb):
-        pass
